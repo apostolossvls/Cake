b79710ab70cb630ac3700fe00fd6f467fe618e98
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-03-25 02:30:46 +0200

    Cake movement
    Cake move around the scene with a small jump added

--- Cake/Assets/Scripts/Player/Movement.cs
+++ Cake/Assets/Scripts/Player/Movement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    Rigidbody rig;
+    public float moveSpeed = 5f;
+    public float moveJumpForce = 10f;
+    Vector3 movement;
+    public Transform GroundCheckPivot;
+
+    void Start(){
+        if (rig==null) rig = GetComponentInChildren<Rigidbody>(); 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate(){
+        if (movement != Vector3.zero)
+        {
+            if (IsGrounded()){
+                rig.AddForce(transform.up * moveJumpForce, ForceMode.Impulse);
+            }
+            else Debug.Log("On air");
+            rig.MovePosition(rig.position +  movement * moveSpeed * Time.fixedDeltaTime);
+
+        }
+        //Debug.DrawRay(new Vector3(transform.position.x, transform.position.y-distToGround, transform.position.z), -transform.up, Color.red);
+    }
+
+    public bool IsGrounded() {
+        if (rig.velocity.y==0) return true;
+        //else return Physics.Raycast(new Vector3(transform.position.x, transform.position.y-distToGround, transform.position.z), Vector3.down, 0.2f);
+        else {
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(GroundCheckPivot.position, -Vector3.up, 0.1f);
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.transform.root!=transform){
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }

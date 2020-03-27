d5aa8627339b07713303f86e50743f840ad7b7d5
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-03-27 19:13:38 +0200

    Cake movement
    Cake is moving on 3d space with WASD and jumps with Space.
    Cake performs 2 type of bites. One grounded and one on air.

---
+++ Cake/Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody rig;
+    public Transform normalBitePivot;
+    public float biteForwardForce = 2f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            if (!IsGroundedBite()){
+                animator.ResetTrigger("GroundBite");
+                animator.SetTrigger("Bite");
+            }
+            else {
+                animator.ResetTrigger("Bite");
+                animator.SetTrigger("GroundBite");
+            }
+            if (Mathf.Abs(rig.velocity.x) < 1) rig.AddForce(transform.forward * biteForwardForce, ForceMode.Impulse);
+        }
+    }
+
+    public bool IsGroundedBite() {
+        if (rig.velocity.y==0) return true;
+        //else return Physics.Raycast(new Vector3(transform.position.x, transform.position.y-distToGround, transform.position.z), Vector3.down, 0.2f);
+        else {
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(normalBitePivot.position, -Vector3.up, 1.5f);
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.transform.root!=transform){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- Cake/Assets/Scripts/Player/Movement.cs
+++ Cake/Assets/Scripts/Player/Movement.cs
@@ -7,23 +7,50 @@ public class Movement : MonoBehaviour
     Rigidbody rig;
     public float moveSpeed = 5f;
     public float rotationSpeed = 5f;
+    public float jumpForce = 5f;
     public float moveJumpForce = 10f;
     Vector3 movement;
-    public Transform GroundCheckPivot;
+    public Transform groundCheckPivot;
+    public Transform runningGroundCheckPivot;
     public Transform cam;
+    public bool runningJump;
+    public bool jumpPressed;
+    bool onJump;
+    bool onJumpUpwards;
 
     void Start(){
         Cursor.visible = false;
         if (rig==null) rig = GetComponentInChildren<Rigidbody>(); 
+
+        onJump = false;
+        jumpPressed = false;
+        runningJump = false;
+        onJumpUpwards = false;
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.z = Input.GetAxisRaw("Vertical");
+        if (movement.x != 0 || movement.z !=0){
+
+        }
+
+        jumpPressed = Input.GetButton("Jump");
     }
 
     void FixedUpdate(){
+        //JUMP
+        if (jumpPressed){
+            if (IsGrounded() || (runningJump && IsGroundedRunning() && !onJumpUpwards)){
+                jumpPressed = false;
+                onJump = true;
+                onJumpUpwards = true;
+                rig.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+
+        //position
         if (movement != Vector3.zero)
         {
             Vector3 targetDirection = new Vector3(movement.x, 0f, movement.z);
@@ -33,21 +60,49 @@ public class Movement : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDirection), Time.deltaTime * rotationSpeed);
             if (IsGrounded()){
                 rig.AddForce(transform.up * moveJumpForce, ForceMode.Impulse);
+                runningJump = true;
             }
             //else Debug.Log("On air");
             //rig.MovePosition(rig.position + new Vector3(forward.x * movement.x, 0, forward.z * movement.z) * moveSpeed * Time.fixedDeltaTime);
             rig.MovePosition(rig.position + targetDirection * moveSpeed * Time.fixedDeltaTime);
             Debug.DrawRay(transform.position, new Vector3(cam.forward.x, 0, cam.forward.z).normalized * 10f, Color.blue);
         }
+
+        //checks
+        if (!IsGroundedRunning()){
+            if (onJump && onJumpUpwards) {
+                onJumpUpwards = false;
+            }
+        }
         //Debug.DrawRay(new Vector3(transform.position.x, transform.position.y-distToGround, transform.position.z), -transform.up, Color.red);
     }
 
     public bool IsGrounded() {
+        if (runningJump) runningJump = false;
+        if (onJump) onJump = false;
+        if (onJumpUpwards) onJumpUpwards = false;
         if (rig.velocity.y==0) return true;
         //else return Physics.Raycast(new Vector3(transform.position.x, transform.position.y-distToGround, transform.position.z), Vector3.down, 0.2f);
         else {
             RaycastHit[] hits;
-            hits = Physics.RaycastAll(GroundCheckPivot.position, -Vector3.up, 0.1f);
+            hits = Physics.RaycastAll(groundCheckPivot.position, -Vector3.up, 0.1f);
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.transform.root!=transform){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public bool IsGroundedRunning() {
+        if (runningJump) runningJump = false;
+        if (rig.velocity.y==0) return true;
+        //else return Physics.Raycast(new Vector3(transform.position.x, transform.position.y-distToGround, transform.position.z), Vector3.down, 0.2f);
+        else {
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(runningGroundCheckPivot.position, -Vector3.up, 0.5f);
             foreach (RaycastHit hit in hits)
             {
                 if (hit.collider.transform.root!=transform){

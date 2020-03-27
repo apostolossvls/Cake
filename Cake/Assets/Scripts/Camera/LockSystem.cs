db2939d54cbea205947b9de6dabd641223f0aeb2
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-03-27 23:56:38 +0200

    Lock camera switching
    By pressing the middle mouse button. Player switches between "FreeLook" and "Locked on target" cameras

---
+++ Cake/Assets/Scripts/Camera/LockSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class LockSystem : MonoBehaviour
+{
+    public bool onLock;
+    public CinemachineVirtualCameraBase mainVcam;
+    public CinemachineVirtualCameraBase lockVcam;
+    public int mainPriority;
+    void Start()
+    {
+        mainPriority = mainVcam.GetComponent<CinemachineFreeLook>().Priority;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(2)){
+            onLock = !onLock;
+
+            if (onLock){
+                lockVcam.GetComponent<CinemachineVirtualCamera>().Priority = mainPriority + 1;
+            }
+            else {
+                lockVcam.GetComponent<CinemachineVirtualCamera>().Priority = mainPriority - 1;
+            }
+        }
+    }
+}

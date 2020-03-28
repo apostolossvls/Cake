45571b1145b00dd8f24aa85b0e16c7c83ac28bc8
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-03-28 21:23:40 +0200

    Switch locking
    When locking the camera, the script searches for the most fitted target.

--- Cake/Assets/Scripts/Camera/LockSystem.cs
+++ Cake/Assets/Scripts/Camera/LockSystem.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Animations;
 
 public class LockSystem : MonoBehaviour
 {
     public bool onLock;
+    public Transform player;
+    public Transform target;
     public CinemachineVirtualCameraBase mainVcam;
     public CinemachineVirtualCameraBase lockVcam;
+    public LayerMask layerMask;
+    public Transform lockIcon;
+    public float lockRadius = 20f;
+    public float lockAngle =  15.0f;
     public int mainPriority;
+    ConstraintSource constraintSource;
+
     void Start()
     {
         mainPriority = mainVcam.GetComponent<CinemachineFreeLook>().Priority;
@@ -18,13 +27,61 @@ public class LockSystem : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(2)){
             onLock = !onLock;
+            target = FindTarget();
+            SwitchCamera(target);
+        }
 
-            if (onLock){
-                lockVcam.GetComponent<CinemachineVirtualCamera>().Priority = mainPriority + 1;
+        if (lockIcon && target!=player){
+            lockIcon.rotation = transform.rotation * Quaternion.Euler(-90f, 0f, 0f);
+        }
+    }
+
+    Transform FindTarget(){
+        Transform t = player;
+
+        Collider[] hitColliders = Physics.OverlapSphere(player.position, lockRadius, layerMask);
+
+        float dis = int.MaxValue;
+
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            //FOR EACH OBJECT
+            Vector3 targetDir = hitColliders[i].transform.position - transform.position;
+            float angle = Vector3.Angle(targetDir, transform.forward);
+            if (angle < lockAngle){
+                float tempDis = Vector3.Distance(player.position, hitColliders[i].transform.position);
+                if (tempDis < dis){
+                    t = hitColliders[i].transform;
+                    dis = tempDis;
+                }
             }
-            else {
-                lockVcam.GetComponent<CinemachineVirtualCamera>().Priority = mainPriority - 1;
+
+            i++;
+        }
+
+        return t;
+    }
+
+    void SwitchCamera(Transform t){
+        if (onLock && t){
+            if (t!=player){
+                lockIcon.gameObject.SetActive(true);
+                PositionConstraint pc = lockIcon.GetComponent<PositionConstraint>();
+                if (pc){
+                    if (pc.sourceCount > 0) pc.RemoveSource(0);
+                    constraintSource.sourceTransform = t;
+                    constraintSource.weight = 1;
+                    pc.AddSource(constraintSource);
+                    //pc.translationOffset = Vector3.zero;
+                    //pc.translationAtRest = Vector3.zero;
+                }
+                lockVcam.GetComponent<CinemachineVirtualCamera>().Priority = mainPriority + 1;
+                return;
             }
         }
+
+        lockIcon.gameObject.SetActive(false);
+        lockVcam.GetComponent<CinemachineVirtualCamera>().Priority = mainPriority - 1;
     }
 }

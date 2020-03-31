9e765cd455adda0e530b7c331f409edf58df8fa9
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-03-31 23:10:15 +0300

    Eyes popping on health system
    When health gets lower that 2/3 and 1/3 of the max health, an eye pops out of its place.

--- Cake/Assets/Scripts/Player/PlayerHealth.cs
+++ Cake/Assets/Scripts/Player/PlayerHealth.cs
@@ -4,24 +4,62 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float maxHealth;
+    public float maxHealth = 3f;
     public float health;
     public Animator animator;
+    public Transform eyeRight;
+    public Transform eyeLeft;
+    bool hasEyeRight;
+    bool hasEyeLeft;
+
     void Start()
     {
         health = maxHealth;
+        hasEyeRight = true;
+        hasEyeLeft = true;
+        //Debug.Log("(2/3) * maxHealth: "+ (2/3) * maxHealth+ " , (1/3) * maxHealth: "+(1/3) * maxHealth);
     }
 
     void Update()
     {
+        if (health <= (2f/3f) * maxHealth && hasEyeLeft){
+            SeperateEye(false, true);
+        }
+        if (health <= (1f/3f) * maxHealth && hasEyeRight){
+            SeperateEye(true, false);
+        }
         if (health<=0){
             BeginDeath();
         }
     }
 
+    public void SeperateEye(bool right = true, bool left = true){
+        Debug.Log("separation "+right+" , "+left);
+        if (right){
+            hasEyeRight = false;
+            
+            GameObject g = GameObject.Instantiate(eyeRight.gameObject, eyeRight.position, Quaternion.identity);
+            g.GetComponent<Collider>().isTrigger = false;
+            Rigidbody r = g.AddComponent(typeof(Rigidbody)) as Rigidbody;
+            r.mass = 0.01f;
+
+            eyeRight.GetComponent<Renderer>().enabled = false;
+        }
+        if (left){
+            hasEyeLeft = false;
+
+            GameObject g = GameObject.Instantiate(eyeLeft.gameObject, eyeLeft.position, Quaternion.identity);
+            g.GetComponent<Collider>().isTrigger = false;
+            Rigidbody r = g.AddComponent(typeof(Rigidbody)) as Rigidbody;
+            r.mass = 0.01f;
+            
+            eyeLeft.GetComponent<Renderer>().enabled = false;
+        }
+    }
+
     public void BeginDeath(){
         if (!animator){
-            BeginDeath();
+            Die();
         }
         else {
             animator.SetBool("Death", true);

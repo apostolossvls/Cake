5a22e8d1e5ae88a642b64e764e92fd157492f85e
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2020-04-08 19:35:15 +0300

    Cake birth
    Cake (player) special interaction with a whole cake. Upon start, player is within the cake. By pressing the movement and jump buttons the players stragles to come out of the cake. After pressing repeatadly the players gets seperated and becomes the original character. Until then the player doesn't have control over the cake (original character).

---
+++ Cake/Assets/Scripts/Player/WholeCake.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WholeCake : MonoBehaviour
+{
+    public float powerNeeded = 10;
+    public float unitsPerPress = 1;
+    public float power;
+    public bool attached = true;
+    public GameObject Player;
+    public GameObject Cake100;
+    public GameObject cake90;
+    public GameObject particles;
+    public Animator animator;
+
+    void Start()
+    {
+        power = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!attached) return;
+        bool pressed = false;
+        if (power > 0) power -= Time.deltaTime;
+        if (power < 0) power = 0;
+        pressed = Input.GetButtonDown("Horizontal")? true : pressed;
+        pressed = Input.GetButtonDown("Vertical") ? true : pressed;
+        pressed = Input.GetButtonDown("Jump") ? true : pressed;
+        if (pressed)
+        {
+            Stragle();
+        }
+        if (power >= powerNeeded)
+        {
+            Detach();
+        }
+    }
+
+    void Stragle()
+    {
+        power += unitsPerPress;
+        animator.SetFloat("StragleRandom", Random.Range(0f, 1f));
+        animator.SetTrigger("Stragle");
+    }
+
+    void Detach()
+    {
+        attached = false;
+        Cake100.SetActive(false);
+        cake90.SetActive(true);
+        particles.SetActive(true);
+        Player.transform.SetParent(null);
+        Player.SetActive(true);
+    } 
+}
